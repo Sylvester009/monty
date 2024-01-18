4f85721bc8fdbc8d7cb2c1526b8c5bbe3e8d5a6f
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 19:38:23 +0100

    Update stack_operator.c

--- stack_operator.c
+++ stack_operator.c
@@ -19,7 +19,7 @@ printf("%d\n", (*head)->n);
 }
 
 /**
-* f_pop - removes the top element of the stack.
+* pop - removes the top element of the stack.
 * @head: head of stack
 * @line_count: counts line
 * Return: void
@@ -42,32 +42,32 @@ free(h);
 }
 
 /**
-* swap - adds the top two elements of the stack.
+* swap - swaps the top two elements of the stack.
 * @head: head of stack
- * @line_count: counts line
- * Return: void
+* @line_count: counts line
+* Return: void
 */
 void swap(stack_t **head, unsigned int line_count)
 {
-	stack_t *h;
-	int len = 0, tmp;
+stack_t *h;
+int len = 0, tmp;
 
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		len++;
-	}
-	if (len < 2)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
-		fclose(bag.file_pointer);
-		free(bag.line_content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	tmp = h->n;
-	h->n = h->next->n;
-	h->next->n = tmp;
+h = *head;
+while (h)
+{
+h = h->next;
+len++;
+}
+if (len < 2)
+{
+fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+fclose(bag.file_pointer);
+free(bag.line_content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+tmp = h->n;
+h->n = h->next->n;
+h->next->n = tmp;
 }
