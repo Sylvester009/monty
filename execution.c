c5972d9d2e3e36598c369878131106cb5d593c16
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 13:59:05 +0100

    Create execution.c

---
+++ execution.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* execute - function that executes the opcode
+* @stack: head stack linked list
+* @line_number: line count
+*
+* Return: nothing
+*/
+
+void execute(stack_t **stack, unsigned int line_number)
+{
+char *opcode;
+int value;
+  
+if (sscanf(bag.line_content, "%ms %d", &opcode, &value) == 2)
+{
+if (strcmp(opcode, "push") == 0)
+{
+push(stack, value);
+}
+else if (strcmp(opcode, "pall") == 0)
+{
+pall(stack, line_number);
+}
+else
+{
+fprintf(stderr, "Unknown opcode '%s' at line %u\n", opcode, line_number);
+exit(EXIT_FAILURE);
+}
+
+free(opcode);
+}
+else
+{
+fprintf(stderr, "Invalid instruction format at line %u\n", line_number);
+exit(EXIT_FAILURE);
+}
+}
