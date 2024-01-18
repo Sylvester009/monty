12d244f7892ec9aa65dcd5d0f98d955a4abdb3b2
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 19:47:09 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -68,7 +68,6 @@ void pall(stack_t **head, unsigned int line_count);
 /*stack operators*/
 void pint(stack_t **head, unsigned int line_count);
 void pop(stack_t **head, unsigned int line_count);
-void swap(stack_t **head, unsigned int line_count);
 
 /*Execution file*/
 int execute(char *line_content, stack_t **stack, unsigned int line_count, FILE *file_pointer);
