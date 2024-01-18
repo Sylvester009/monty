7e520dc1f73a03bcc957866edf4ce082eec11dba
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 19:35:51 +0100

    added swap

--- stack_operator.c
+++ stack_operator.c
@@ -40,3 +40,34 @@ h = *head;
 *head = h->next;
 free(h);
 }
+
+/**
+* swap - adds the top two elements of the stack.
+* @head: head of stack
+ * @line_count: counts line
+ * Return: void
+*/
+void swap(stack_t **head, unsigned int line_count)
+{
+	stack_t *h;
+	int len = 0, tmp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bag.file_pointer);
+		free(bag.line_content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	tmp = h->n;
+	h->n = h->next->n;
+	h->next->n = tmp;
+}
