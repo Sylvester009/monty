c608eb20b90cb65775f9537b6722361f858abf06
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 15:45:48 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -63,8 +63,8 @@ extern bag_t bag;
 
 
 /**pull and pall*/
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+void push(stack_t **head, unsigned int line_count);
+void pall(stack_t **head, unsigned int line_count);
 
 /*Execution file*/
 void execute(stack_t **stack, unsigned int line_number);
