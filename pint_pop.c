723e482f387502335e4b5bc80abd7f548fbdfa54
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 19:48:30 +0100

    Update and rename pint.c to pint_pop.c

--- pint_pop.c
+++ pint_pop.c
@@ -17,3 +17,26 @@ exit(EXIT_FAILURE);
 }
 printf("%d\n", (*head)->n);
 }
+
+/**
+* pop - removes the top element of the stack.
+* @head: head of stack
+* @line_count: counts line
+* Return: void
+*/
+void pop(stack_t **head, unsigned int line_count)
+{
+stack_t *h;
+
+if (*head == NULL)
+{
+fprintf(stderr, "L%d: can't pop an empty stack\n",line_count);
+fclose(bag.file_pointer);
+free(bag.line_content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+h = *head;
+*head = h->next;
+free(h);
+}
