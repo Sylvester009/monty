06430fdc12b4064bae8372f787fb7a8418dde469
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 16:05:26 +0100

    Update handlers.c

--- handlers.c
+++ handlers.c
@@ -1,12 +1,60 @@
 #include "monty.h"
-
-char *my_strdup(const char *str)
+/**
+ * addnode - adds node to the head stack
+ * @head: stack head
+ * @n: new value
+ * Return: void
+*/
+void add_node(stack_t **head, int n)
 {
-size_t len = strlen(str);
-char *copy = malloc(len + 1);
-if (copy == NULL) {
-return (NULL);
+
+	stack_t *new_node, *tmp;
+
+	tmp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (tmp)
+		tmp->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
 }
-strcpy(copy, str);
-return (copy);
+
+**
+ * addqueue - adds node to the tail stack
+ * @n: new value
+ * @head: head of the stack
+ * Return: void
+*/
+  
+void add_queue(stack_t **head, int n)
+{
+	stack_t *new_node, *tmp;
+
+	tmp = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		printf("Error\n");
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (tmp)
+	{
+		while (tmp->next)
+			tmp = tmp->next;
+	}
+	if (tmp == NULL)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		tmp->next = new_node;
+		new_node->prev = tmp;
+	}
 }
