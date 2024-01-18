be1833ca594107b49f327e7fb17ed49b549013e0
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 20:07:28 +0100

    Create nop.c

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+*nop- doesn’t do anything.
+*@head: head of stack
+*@line_count: counts line
+*Return: void
+*/
+void nop(stack_t **head, unsigned int line_count)
+{
+(void) line_count;
+(void) head;
+}
