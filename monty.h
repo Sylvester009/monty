05d93df097c0ba565b10dfbf5e57c3406608f697
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 19:36:05 +0100

    Update the header file

--- monty.h
+++ monty.h
@@ -65,9 +65,10 @@ extern bag_t bag;
 /**pull and pall*/
 void push(stack_t **head, unsigned int line_count);
 void pall(stack_t **head, unsigned int line_count);
-/*pint and pop*/
+/*stack operators*/
 void pint(stack_t **head, unsigned int line_count);
 void pop(stack_t **head, unsigned int line_count);
+void swap(stack_t **head, unsigned int line_count);
 
 /*Execution file*/
 int execute(char *line_content, stack_t **stack, unsigned int line_count, FILE *file_pointer);
