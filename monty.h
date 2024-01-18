73c9ebe901eb8033366ee295dc94c456aa28dc77
Sylvester009 <samsylvester09@gmail.com>
2024-01-18 03:45:29 -0800

    push&pall files

--- monty.h
+++ monty.h
@@ -38,3 +38,10 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+
+/**pull and pall*/
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+#endif
