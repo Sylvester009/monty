9e61f1258c564262a86c7c745843dc32c9f7b4bf
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 13:17:02 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+struct
+{
+char *current_line;
+} uni_data;
+
+bag_t bag = {NULL, NULL, NULL, 0};
+
+int main(int argc, char *argv[])
+{
+stack_t *stack = NULL;
+
+bag.argument = "10";
+bag.file_pointer = fopen(argv[1], "r");
+bag.line_content = "push 10";
+bag.stack_queue_flg = 0;
+
+uni_data.current_line = bag.line_content;
+push(&stack, 1);
+
+uni_data.current_line = "pall";
+pall(&stack, 2);
+
+return (0);
+}
