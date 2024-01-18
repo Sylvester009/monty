7f7f8ecd567eb171b6065be0a8ae8405f2cff133
Samuel Sylvester <samsylvester09@gmail.com>
2024-01-18 19:27:19 +0100

    create pint and pop

--- stack_operator.c
+++ stack_operator.c
@@ -1,3 +1,23 @@
+#include "monty.h"
+/**
+* pint - prints the value at the top of the stack
+* @head: head of stack
+* @line_count: counts line
+* Return: void
+*/
+void pint(stack_t **head, unsigned int line_count)
+{
+if (*head == NULL)
+{
+fprintf(stderr, "L%u: can't pint, stack empty\n", line_count);
+fclose(bag.file_pointer);
+free(bag.line_content);
+free_stack(*head);
+exit(EXIT_FAILURE);
+}
+printf("%d\n", (*head)->n);
+}
+
 /**
 * f_pop - removes the top element of the stack.
 * @head: head of stack
